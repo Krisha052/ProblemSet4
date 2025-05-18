94a410125c8de0bcd6ec49afd12812491c8caa41
Krisha052 <165743536+Krisha052@users.noreply.github.com>
2025-05-18 16:58:46 -0400

    Add files via upload

---
+++ adieu.py
@@ -0,0 +1,21 @@
+
+def main():
+    names = []
+    while True:
+        try:
+            name = input("Name: ")
+            names.append(name)
+        except EOFError:
+            print()
+            if len(names) == 1:
+                print(f"Adieu, adieu, to {names[0]}")
+            elif len(names) == 2:
+                print(f"Adieu, adieu, to {names[0]} and {names[1]}")
+            else:
+                # All but the last two joined with commas
+                print(f"Adieu, adieu, to {', '.join(names[:-1])}, and {names[-1]}")
+            break
+
+if __name__ == "__main__":
+    main()
+
